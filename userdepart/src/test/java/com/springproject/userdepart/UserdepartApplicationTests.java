9137049fb92cf7b563ac832f15fdd13e4052eccc
CarlosRyan07 <carlosryan.profissional@gmail.com>
2024-03-11 22:23:57 -0300

    .

---
+++ userdepart/src/test/java/com/springproject/userdepart/UserdepartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproject.userdepart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdepartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
